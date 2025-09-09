ef0ec7990ca03b481f3c8b8c71cab1668bbda7fe
Ovidijus <ovismc123@gmail.com>
2025-09-09 22:16:32 +0300

    feat: created a StatusHistory model

---
+++ PackageTracker.Server/Models/StatusHistory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PackageTracker.Server.Models
+{
+    public class StatusHistory
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string PrevStatus { get; set; }
+
+        public string NewStatus { get; set; }
+
+        public string DateModified { get; set; }
+    }
+}
